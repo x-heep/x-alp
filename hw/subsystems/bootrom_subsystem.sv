a556c1ed76223a1a4ef2b109146a9168ef201b5b
Luigi Giuffrida <luigi.giuffrida.98@gmail.com>
2026-01-22 18:16:56 +0100

    Fix bootrom address space

--- hw/subsystems/bootrom_subsystem.sv
+++ hw/subsystems/bootrom_subsystem.sv
@@ -1,5 +1,3 @@
-
-
 module bootrom_subsystem #(
     parameter type reg_req_t = logic,
     parameter type reg_rsp_t = logic
@@ -11,14 +9,16 @@ module bootrom_subsystem #(
     import core_v_mcu_pkg::*;
 
     bootrom #(
-        .AddrWidth(64),
+        .AddrWidth(32),
         .DataWidth(32)
     ) u_bootrom (
         .clk_i ('0),
         .rst_ni(1'b1),
         .req_i (reg_req_i.valid),
-        .addr_i(reg_req_i.addr),
+        .addr_i(reg_req_i.addr[31:0]),
         .data_o(reg_rsp_o.rdata)
     );
+
+    assign reg_rsp_o.ready = 1'b1;
 
 endmodule
