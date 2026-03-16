cd29e39e49bd480b70505595ed092e08e312232e
Luigi Giuffrida <luigi.giuffrida.98@gmail.com>
2026-03-16 20:31:51 +0100

    Cleanup files

--- hw/vendor/pulp_platform/register_interface/src/reg_intf.sv
+++ hw/vendor/pulp_platform/register_interface/src/reg_intf.sv
@@ -20,24 +20,24 @@
 /// read data and error signals. These signals must be constant while valid and
 /// ready are both high.
 interface REG_BUS #(
-    /// The width of the address.
-    parameter int ADDR_WIDTH = -1,
-    /// The width of the data.
-    parameter int DATA_WIDTH = -1
-) (
-    input logic clk_i
+  /// The width of the address.
+  parameter int ADDR_WIDTH = -1,
+  /// The width of the data.
+  parameter int DATA_WIDTH = -1
+)(
+  input logic clk_i
 );
 
-    logic [  ADDR_WIDTH-1:0] addr;
-    logic                    write;  // 0=read, 1=write
-    logic [  DATA_WIDTH-1:0] rdata;
-    logic [  DATA_WIDTH-1:0] wdata;
-    logic [DATA_WIDTH/8-1:0] wstrb;  // byte-wise strobe
-    logic                    error;  // 0=ok, 1=error
-    logic                    valid;
-    logic                    ready;
+  logic [ADDR_WIDTH-1:0]   addr;
+  logic                    write; // 0=read, 1=write
+  logic [DATA_WIDTH-1:0]   rdata;
+  logic [DATA_WIDTH-1:0]   wdata;
+  logic [DATA_WIDTH/8-1:0] wstrb; // byte-wise strobe
+  logic                    error; // 0=ok, 1=error
+  logic                    valid;
+  logic                    ready;
 
-    modport in(input addr, write, wdata, wstrb, valid, output rdata, error, ready);
-    modport out(output addr, write, wdata, wstrb, valid, input rdata, error, ready);
+  modport in  (input  addr, write, wdata, wstrb, valid, output rdata, error, ready);
+  modport out (output addr, write, wdata, wstrb, valid, input  rdata, error, ready);
 
 endinterface
