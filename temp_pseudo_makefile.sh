4be1c24ef64ca0907e49bd57b55da3b45c23a1ed
alessandro.barocci <62031564+alebaroc@users.noreply.github.com>
2026-04-18 02:10:56 +0200

    + Makefile : reg-gen target : improved portability + pseudo_makefile.sh : updated with new xheep_spi makefile changes + data/spi_subsystem.hjson : added + data/w25q128jw_controller.hjson : added + xheep_spi.vendor.hjson : updated to ignore more files ; now spi/ inside xheep/ directory
    (xheep/spi changes in next commit)

--- temp_pseudo_makefile.sh
+++ temp_pseudo_makefile.sh
@@ -3,29 +3,23 @@
 # this is a pseudo-makefile, temporary makefile substitute, not to be pushed (not final solution).
 # current makefile doesn't support these instructions as targets
 
-#!/bin/bash
-
-# pseudo-makefile script
-
-# usage: ./temp_pseudo_makefile.sh spi-vendor spi-gen
+# usage: ./temp_pseudo_makefile.sh vendor periph-gen
 
 for arg in "$@"
 do
   case $arg in
 
-    # vendor xheep_spi
-    spi-vendor)
+    # vendor xheep/spi
+    vendor)
       echo "Vendoring xheep_spi..."
       python ./util/vendor.py ./hw/vendor/xheep_spi.vendor.hjson -vU
       ;;
 
-    # xheep_spi/rtl/spi_subsystem.sv generation
-    spi-gen)
+    # vendor/xheep/spi/rtl/spi_subsystem.sv generation
+    periph-gen)
       echo "Generating spi_subsystem.sv..."
-      currdir=$(pwd)
-      cd hw/vendor/spi || exit
-      make gen-spi
-      cd "$currdir" || exit
+      make -C  hw/vendor/xheep/spi periph-gen \
+      SPI_SUBSYS_PERIPH_GEN=axi
       ;;
 
     *)
