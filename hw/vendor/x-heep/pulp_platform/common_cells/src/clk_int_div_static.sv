1c923aa49591531741671bb9913d9968a17d170c
Luigi Giuffrida <luigi.giuffrida.98@gmail.com>
2026-06-04 16:44:26 +0200

    revendor x-heep

--- hw/vendor/x-heep/pulp_platform/common_cells/src/clk_int_div_static.sv
+++ hw/vendor/x-heep/pulp_platform/common_cells/src/clk_int_div_static.sv
@@ -58,38 +58,38 @@
 
 
 module clk_int_div_static #(
-    parameter int unsigned DIV_VALUE = 1,
-    parameter bit ENABLE_CLOCK_IN_RESET = 1'b1
+  parameter int unsigned DIV_VALUE = 1,
+  parameter bit ENABLE_CLOCK_IN_RESET = 1'b1
 ) (
-    input  logic clk_i,
-    input  logic rst_ni,
-    input  logic en_i,
-    input  logic test_mode_en_i,
-    output logic clk_o
+  input logic  clk_i,
+  input logic  rst_ni,
+  input logic  en_i,
+  input logic  test_mode_en_i,
+  output logic clk_o
 );
-    if (DIV_VALUE == 0) begin : gen_elab_error
-        $error("DIV_VALUE must be strictly larger than 0.");
-    end
+  if (DIV_VALUE == 0) begin : gen_elab_error
+    $error("DIV_VALUE must be strictly larger than 0.");
+  end
 
-    localparam int unsigned DivValueWidth = $clog2(DIV_VALUE + 1);
+  localparam int unsigned DivValueWidth = $clog2(DIV_VALUE+1);
 
-    logic [DivValueWidth-1:0] div_value;
-    assign div_value = DIV_VALUE;
+  logic [DivValueWidth-1:0] div_value;
+  assign div_value = DIV_VALUE;
 
-    clk_int_div #(
-        .DIV_VALUE_WIDTH      (DivValueWidth),
-        .DEFAULT_DIV_VALUE    (DIV_VALUE),
-        .ENABLE_CLOCK_IN_RESET(ENABLE_CLOCK_IN_RESET)
-    ) i_clk_int_div (
-        .clk_i,
-        .rst_ni,
-        .en_i,
-        .test_mode_en_i,
-        .div_i       (div_value),
-        .div_valid_i (1'b0),
-        .div_ready_o (),
-        .clk_o,
-        .cycl_count_o()
-    );
+  clk_int_div #(
+    .DIV_VALUE_WIDTH       ( DivValueWidth         ),
+    .DEFAULT_DIV_VALUE     ( DIV_VALUE             ),
+    .ENABLE_CLOCK_IN_RESET ( ENABLE_CLOCK_IN_RESET )
+  ) i_clk_int_div (
+    .clk_i,
+    .rst_ni,
+    .en_i,
+    .test_mode_en_i,
+    .div_i        ( div_value ),
+    .div_valid_i  ( 1'b0      ),
+    .div_ready_o  (           ),
+    .clk_o,
+    .cycl_count_o (           )
+  );
 
 endmodule
